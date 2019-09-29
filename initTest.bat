45c0a1006b76b08169d2cad89770049c3f058237
benjaminl <benjamin.li@ehealth.com>
2019-09-29 16:04:29 +0800

    add init for dev and test

--- initTest.bat
+++ initTest.bat
@@ -1,1 +1,1 @@
-mongo 127.0.0.1/firestone-test --eval "db.dropDatabase(); db.getSiblingDB('firestone-test');" "c:/aqua/firestone-server/initTestData.js"
+mongo 127.0.0.1/firestone-test --eval "db.dropDatabase(); db.getSiblingDB('firestone-test');" "c:/aqua/firestone-server/initData.js"
