deff0fb30de010dc91b4de79587674d10a6dc8fe
benjaminl <benjamin.li@ehealth.com>
2019-11-14 17:18:59 +0800

    add deployment

--- shell/runfirerock.bat
+++ shell/runfirerock.bat
@@ -1,4 +1,4 @@
 @echo off
-cd %FR_ENG_HOME%
-pipenv run firerock %1 -v -m
-REM pipenv run firerock %1 -m --hours 18 --minutes * -v -i --date 2019-10-30-m
+cd %cd%\shell
+calculate.exe %1 -v -m
+REM calculate.exe %1 -m --hours 18 --minutes * -v -i --date 2019-10-30-m
--- shell/runfirestone.bat
+++ shell/runfirestone.bat
@@ -1,4 +1,4 @@
 @echo off
-cd %FR_ENG_HOME%
-pipenv run firestone 000000 -v --md
-REM pipenv run firestone 000000 --hours 18 --minutes * -v --date 2019-10-30 -m --md
+cd %cd%\shell
+main.exe 000000 -v --md --hours %time:~0,2% --minutes *
+REM main.exe 000000 --hours 18 --minutes * -v --date 2019-10-30 -m --md
