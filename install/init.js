5ac461aa6a6ead9ed2253d56285073caf6d24838
benjaminl <benjamin.li@ehealth.com>
2019-11-25 15:49:15 +0800

    add basic sell strategy

--- install/init.js
+++ install/init.js
@@ -63,4 +63,26 @@ db.strategies.insertMany([{
         },
         "volume" : 100
     }
+},{
+    "_id" : ObjectId("5da19b7d181fc3600c5544c2"),
+    "name" : "基础策略",
+    "description" : "在<i>监控时间</i>范围内,当<i>大盘涨幅</i>处于指定范围，并且当前<i>个股涨幅</i>处于指定范围，则卖出股票",
+    "op" : "sell",
+    "url" : "Basic",
+    "parameters" : {
+        "code" : "",
+        "monitorTime" : {
+            "start" : "09:30",
+            "end" : "15:00"
+        },
+        "index_percent" : {
+            "low" : "-1.0",
+            "high" : "3.0"
+        },
+        "percent" : {
+            "low" : "-0.5",
+            "high" : "2.5"
+        },
+        "volume" : 100
+    }
 }]);
