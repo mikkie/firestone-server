5a1a66d44c283733c7a1ef7f8f3cd032447c710d
Benjamin Li <benjamin.li@ehealth.com>
2020-02-07 20:35:54 +0800

    clean firestone-data

--- install/init.bat
+++ install/init.bat
@@ -1,3 +1,4 @@
 mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "init.js" 
 mongoimport -d firestone -c codes "codes.json"
-mongoimport -d firestone -c concepts "concepts.json" && exit
+mongoimport -d firestone -c concepts "concepts.json" 
+mongo 127.0.0.1/firestone-data --eval "db.getCollectionNames().forEach(function(c) { if (c.indexOf('system.') == -1) db[c].drop(); })" && exit
