3a33d830bebbd9057884d835de0fea644dfdf7c4
benjaminl <benjamin.li@ehealth.com>
2020-02-10 17:40:25 +0800

    drop hot_concept

--- install/init.bat
+++ install/init.bat
@@ -1,4 +1,4 @@
-mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getSiblingDB('firestone');" "init.js" 
+mongo 127.0.0.1/firestone --eval "db.dropDatabase(); db.getCollection('hot_concept').drop(); db.getSiblingDB('firestone');" "init.js" 
 mongoimport -d firestone -c codes "codes.json"
 mongoimport -d firestone -c concepts "concepts.json" 
 mongo 127.0.0.1/firestone-data --eval "db.getCollectionNames().forEach(function(c) { if (c.indexOf('system.') == -1) db[c].drop(); })" && exit
